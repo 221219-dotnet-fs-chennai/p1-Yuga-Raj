742a305086803c24288db22024eb3aecea50e07a
Yugaraj31 <yugarajkumar31@gmail.com>
2023-01-20 12:22:52 +0530

    project complete

---
+++ Project 1/Trainer_project/Trainer_project/MainMenu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using Data;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace trainer
+{
+    public class MainMenu
+
+    {
+
+        public void MainMenuLoop()
+        {
+            //Program.MainLoop mp = new Program.MainLoop();
+            Console.WriteLine("0 - Back");
+            Console.WriteLine("1 - Signup");
+            Console.WriteLine("2 - Login");
+
+            //Dummy
+            Console.WriteLine("3 - Read");
+
+
+
+            int inp = Convert.ToInt32(Console.ReadLine());
+
+            switch (inp)
+            {
+                case 0:
+                    Console.WriteLine("Back pressed");
+
+                    Program.MainLoop = false;
+                    break;
+                case 1:
+                    Console.WriteLine("Sing up menu");
+                    SignUpForm sf = new SignUpForm();
+                    //Program.MainLoop = false;
+                    break;
+                case 2:
+                    Console.WriteLine("Login menu");
+                    LoginForm lf = new LoginForm();
+                    //Program.MainLoop = false;
+                    break;
+                case 3:
+                    SqlHandle sq = new SqlHandle();
+                    DataTable reader = sq.SqlQeryWriterSkillUpdate(@"select * from project.[user]");
+                    //Console.WriteLine(reader);
+                    foreach (DataRow dataRow in reader.Rows)
+                    {
+                        foreach (var item in dataRow.ItemArray)
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine("");
+                    }
+
+                    //Program.MainLoop = false;
+                    break;
+
+
+                default:
+                    Console.WriteLine("Wrong Input");
+                    break;
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Project 1/Trainer_project/User/MyProfile.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Data;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace User 
+{
+
+
+    public class MyProfile
+    {
+        private string Connection = File.ReadAllText(@"C:\Users\SURYA\Training\sqlid.txt");
+
+        public void MyProfile1(int usid)
+        {
+            using SqlConnection con = new SqlConnection(Connection);
+
+            con.Open();
+            string q = $"Select first_name,last_name,email_id,phone_no from project.[user] where user_id = {usid}";
+            using SqlCommand cmd = new SqlCommand(q, con);
+            using SqlDataReader reader = cmd.ExecuteReader();
+            Console.WriteLine("My Profile");
+            while (reader.Read())
+            {
+
+                Console.WriteLine($"First Name - {reader.GetString(0)}");
+                Console.WriteLine($"Last Name - {reader.GetString(1)}");
+                Console.WriteLine($"Email ID - {reader.GetString(2)}");
+                Console.WriteLine($"Phone No - {reader.GetInt32(3)}");
+
+
+            }
+            reader.Close();
+            con.Close();
+        }
+
+        public void MyProfileEdu(int usid)
+        {
+            using SqlConnection conn = new SqlConnection(Connection);
+            conn.Open();
+
+            string q = $"Select * from project.edu where us_id = {usid}";
+            using SqlCommand cmd1 = new SqlCommand(q, conn);
+            using SqlDataReader reader1 = cmd1.ExecuteReader();
+            Console.WriteLine("Education");
+            int countt = 1;
+            while (reader1.Read())
+            {
+                Console.WriteLine($"Education -{countt}");
+                Console.WriteLine("");
+
+                Console.WriteLine($"Institution - {reader1.GetString(1)}");
+                Console.WriteLine($"Course Studied - {reader1.GetString(2)}");
+                Console.WriteLine($"Course Studied from - {reader1.GetDateTime(3)} to {reader1.GetDateTime(4)}");
+                Console.WriteLine($"CGPA score - {reader1.GetString(5)}");
+                countt++;
+
+
+
+            }
+            reader1.Close();
+            conn.Close();
+        }
+
+        public void MyProfileExperience(int usid)
+        {
+            using SqlConnection conn = new SqlConnection(Connection);
+            conn.Open();
+
+            string q = $"Select * from project.comp where us_id = {usid}";
+            using SqlCommand cmd1 = new SqlCommand(q, conn);
+            using SqlDataReader reader1 = cmd1.ExecuteReader();
+            int countt = 1;
+
+            Console.WriteLine("Work Experience");
+            while (reader1.Read())
+            {
+                Console.WriteLine($"Work Experience  - {countt}");
+                Console.WriteLine("");
+                Console.WriteLine($"Job Role - {reader1.GetString(1)}");
+                Console.WriteLine($"Company Name - {reader1.GetString(2)}");
+                Console.WriteLine($"Worked from  {reader1.GetDateTime(3)} to {reader1.GetDateTime(4)}");
+                countt++;
+
+
+
+            }
+            reader1.Close();
+            conn.Close();
+        }
+
+        public void MyProfileSkills(int usid)
+        {
+            using SqlConnection conn = new SqlConnection(Connection);
+            conn.Open();
+
+            string q = $"Select * from project.skills where us_id = {usid}";
+            using SqlCommand cmd1 = new SqlCommand(q, conn);
+            using SqlDataReader reader1 = cmd1.ExecuteReader();
+            int countt = 1;
+
+            Console.WriteLine("Skills");
+            while (reader1.Read())
+            {
+                Console.WriteLine($"{countt} skill - {reader1.GetString(1)}  Experience - {reader1.GetInt32(2) / 12f} Years");
+                Console.WriteLine("");
+
+                countt++;
+
+            }
+            reader1.Close();
+            conn.Close();
+        }
+
+       
+    }
+}
